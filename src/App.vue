b8141aacdd3e868a87380be5dd32f772e287f4eb
benunes01 <be_nunes_pinheiro@hotmail.com>
2020-08-23 00:55:34 -0300

    Montagem do layout do site

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,81 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NameGator</h1>
+    <br>
+    <h6 class="text-secondary">Gerador de nomes</h6>
     </div>
-    <router-view/>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+        <div class="col-md">
+          <h5>Prefixos <span class="badge badge-info">{{ prefixes.length }}</span></h5>
+          <div class="card">
+            <div class="card-body">
+              <ul class="list-group">
+              <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+                {{ prefix }}
+              </li>
+            </ul>
+            </div>
+            <input class="form-control" type="text" placeholder="Digite seu Prefixo">
+          </div>
+        </div>
+        <div class="col-md">
+          <h5>Sufixos <span class="badge badge-info">{{ sufixes.length }}</span></h5>
+          <div class="card">
+
+            <div class="card-body">
+              <ul class="list-group">
+              <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+                {{ sufix }}
+              </li>
+            </ul>
+            </div>
+            <input class="form-control" type="text" placeholder="Digite seu Sufixo">
+          </div>
+        </div>
+      </div>
+      <br>
+      <h5>Dominios <span class="badge badge-info">{{ domains.length }}</span></h5>
+    <div class="card">
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
+                  {{ domain }}
+                </li>
+              </ul>
+            </div>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      prefixes: ['Air', 'Hub', 'Be'],
+      sufixes: ['Cap', 'Food', 'Mac'],
+      domains: ['AirCap', 'AirFood', 'AirMac', 'HubCap'],
+    };
+  },
+};
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#slogan {
+  margin-top: 30px;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#main{
+  background-color: blanchedalmond;
+  padding-top: 30px;
+  padding-bottom: 30px;
 }
 </style>
